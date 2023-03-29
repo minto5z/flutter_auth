16f84fe736c3e8f73eab8859a51ec16861a02061
Mahadiur_Jaman <minto.eatl@gmail.com>
2023-03-29 09:02:19 +0000

    codebase update

--- lib/src/constants/environments.dart
+++ lib/src/constants/environments.dart
@@ -2,12 +2,6 @@ class _Environments {
   final web = 'https://nest-auth.ubbly.club';
   final api = 'https://nest-auth.ubbly.club/api';
   final socket = 'https://nest-auth.ubbly.club/';
-
-  final appleSignInClientId = 'nest-auth.ubbly.club';
-  final appleSignInRedirectUri =
-      Uri.parse('https://nest-auth.ubbly.club/api/auth/apple-callback');
-
-  // final api = 'http://localhost:3000';
 }
 
 _Environments environments = _Environments();
